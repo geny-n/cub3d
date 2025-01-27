e86b48ab459777a916532da8f51fea2682c3e53e
Ninon Geny <ngeny@paul-f5Br6s4.clusters.42paris.fr>
2025-01-27 18:09:18 +0100

    init ninon

--- cub3d.h
+++ cub3d.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cub.h                                              :+:      :+:    :+:   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ngeny <ngeny@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 14:37:21 by ngeny             #+#    #+#             */
-/*   Updated: 2025/01/27 15:50:58 by ngeny            ###   ########.fr       */
+/*   Updated: 2025/01/27 17:44:04 by ngeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,36 @@
 #include "stdlib.h"
 # include "./minilibx-linux/mlx.h"
 
+
+typedef struct s_img
+{
+	char space;
+}	t_item;
+
+typedef struct s_item
+{
+	char space;
+}	t_item;
+
+typedef struct s_player
+{
+	int	x;
+	int	y;
+}	t_item;
+
+typedef struct s_map
+{
+	char **map;
+	int			map_w;
+	int			map_h;
+}	t_data;
+
 typedef struct s_data
 {
 	int i;
+	int			start_x;
+	int			start_y;
+	t_item item;
 }	t_data;
 
 
